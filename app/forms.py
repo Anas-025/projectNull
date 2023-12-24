5aa58e30a43e64d4f0509998acbd183b3963a750
Anas-025 <200280107097@Ldceahm.gujgov.edu.in>
2023-12-24 21:29:42 +0530

    made the api seperated from template view

--- app/forms.py
+++ app/forms.py
@@ -2,15 +2,15 @@ from django import forms
 
 
 choices = [
-    (0, "--select--"),
-    (1, "uppercase"),
+    (0, "Select a format"),
+    (1, "UPPERCASE"),
     (2, "lowercase"),
-    (3, "word count")
+    (3, "Word:3 Count:5")
 ]
 
 
 class MyForm(forms.Form):
 
-    text = forms.CharField(label='Enter text', max_length=10)
+    text = forms.CharField(label="", help_text="", widget=forms.Textarea(attrs={'placeholder': 'Enter text to transform...'}))
     # formatting = forms.IntegerField(label='Enter number')
-    formatting = forms.ChoiceField(choices=choices)
+    formatting = forms.ChoiceField(label="", choices=choices)
--- app/views.py
+++ app/views.py
@@ -1,8 +1,32 @@
 from django.http import HttpResponse
 from .forms import MyForm
 from django.shortcuts import render
-
+import requests 
 
 def index(request):
-    form = MyForm()
-    return render(request, 'form.html', {'form': form})
+    if request.method == 'GET':
+        form = MyForm()
+        return render(request, 'form.html', {'form': form})
+
+    elif request.method == 'POST':
+        form = MyForm(request.POST)
+        csrf_token = request.POST['csrfmiddlewaretoken']
+        headers = {'X-CSRFToken': csrf_token}
+        cookies = {'csrftoken': csrf_token}
+        if form.is_valid():
+            response = requests.post(
+                            'http://127.0.0.1:8000/app/api/util/v1/public/transform',
+                            data={'text': form.cleaned_data['text']},
+                            cookies=cookies,
+                            headers=headers)
+            if response.status_code == 200:
+                transformed_text = response.text
+                return render(request, 'result.html', {'text': transformed_text})
+            else:
+                error_message = f"Error: {response.status_code} - {response.text}"
+                return HttpResponse(error_message)
+        else:
+            return HttpResponse("Invalid form")
+
+    else:
+        return HttpResponse("Invalid request")
